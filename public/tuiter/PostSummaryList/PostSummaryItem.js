baf4bab854e5266c846435c343ad941df82009c9
Jennifer Zajac <zajac.je@northeastern.edu>
2022-10-16 21:18:30 -0400

    a5 fix overlapping text col

--- public/tuiter/PostSummaryList/PostSummaryItem.js
+++ public/tuiter/PostSummaryList/PostSummaryItem.js
@@ -2,7 +2,7 @@ const PostSummaryItem = (post) => {
   return `
   <li class="list-group-item">
     <div class="row">
-      <div class="col-10">
+      <div class="col-9 col-md-10">
         ${post.topic ? `<div class="text-secondary">${post.topic}</div>` : ""}
         <div class="fw-bolder">
           ${post.userName}<i class="fa-solid fa-circle-check ${
@@ -15,7 +15,7 @@ const PostSummaryItem = (post) => {
         </div>
         ${post.tweets ? `<div class="text-secondary">${post.tweets}</div>` : ""}
       </div>
-      <div class="col-2">
+      <div class="col-3 col-md-2">
         <img
           class="float-end wd-tuit-img"
           src=${post.image}
