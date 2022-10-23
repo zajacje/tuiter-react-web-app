144c20710f50505a3aacc11d15c6dd037b7db181
Jennifer Zajac <zajac.je@northeastern.edu>
2022-10-22 23:10:00 -0400

    a6 fix lint. fix navigation and images

--- src/tuiter/explore/index.js
+++ src/tuiter/explore/index.js
@@ -24,17 +24,27 @@ const ExploreComponent = () => {
       </div>
       <ul className="nav nav-pills mb-2">
         <li className="nav-item">
-          <a className="nav-link active">For You</a>
+          <a className="nav-link active" href="/tuiter">
+            For You
+          </a>
         </li>
         <li className="nav-item">
-          <a className="nav-link">Trending</a>
+          <a className="nav-link" href="/trending">
+            Trending
+          </a>
         </li>
         <li className="nav-item">
-          <a className="nav-link">News</a>
+          <a className="nav-link" href="/news">
+            News
+          </a>
         </li>
       </ul>
       <div className="position-relative mb-2">
-        <img src="/images/starship.webp" className="w-100" />
+        <img
+          src="/images/starship.webp"
+          className="w-100"
+          alt={"SpaceX Starship"}
+        />
         <h1 className="position-absolute wd-nudge-up text-white">
           SpaceX Starship
         </h1>
--- src/tuiter/navigation-sidebar/index.js
+++ src/tuiter/navigation-sidebar/index.js
@@ -3,52 +3,62 @@ import React from "react";
 const NavigationSidebar = ({ active = "explore" }) => {
   return (
     <div className="list-group">
-      <a className="list-group-item">Tuiter</a>
+      <a className="list-group-item" href="/tuiter">
+        Tuiter
+      </a>
       <a
         className={`list-group-item
                     ${active === "home" ? "active" : ""}`}
+        href="home.html"
       >
         Home
       </a>
       <a
         className={`list-group-item
                     ${active === "explore" ? "active" : ""}`}
+        href="explore/"
       >
         Explore
       </a>
       <a
         className={`list-group-item
                     ${active === "notifications" ? "active" : ""}`}
+        href="notifications/"
       >
         Notifications
       </a>
       <a
         className={`list-group-item
                     ${active === "messages" ? "active" : ""}`}
+        href="messages/"
       >
         Messages
       </a>
       <a
         className={`list-group-item
                     ${active === "bookmarks" ? "active" : ""}`}
+        href="bookmarks/"
       >
         Bookmarks
       </a>
       <a
         className={`list-group-item
                     ${active === "lists" ? "active" : ""}`}
+        href="lists/"
       >
         Lists
       </a>
       <a
         className={`list-group-item
                     ${active === "profile" ? "active" : ""}`}
+        href="profile.html"
       >
         Profile
       </a>
       <a
         className={`list-group-item
                     ${active === "more" ? "active" : ""}`}
+        href="more/"
       >
         More
       </a>
