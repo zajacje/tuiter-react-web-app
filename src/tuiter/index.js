888364bb110c0c0f2de885cc5405200e80337c9c
Jennifer Zajac <zajac.je@northeastern.edu>
2022-10-22 21:04:39 -0400

    a6 finished lab and started tuiter

---
+++ src/tuiter/index.js
@@ -0,0 +1,16 @@
+import Nav from "../nav";
+import NavigationSidebar from "./navigation-sidebar";
+import WhoToFollowListItem from "./who-to-follow-list/who-to-follow-list-item";
+
+function Tuiter() {
+  return (
+    <div>
+      <Nav />
+      <NavigationSidebar active="home" />
+      <WhoToFollowListItem />
+      <h1>Tuiter</h1>
+    </div>
+  );
+}
+
+export default Tuiter;
