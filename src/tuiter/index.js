a7263199f9ccaa459da73179f9ccd7ccf9ced655
Jennifer Zajac <zajac.je@northeastern.edu>
2022-10-22 22:30:12 -0400

    a6 finish tuiter

--- src/tuiter/index.js
+++ src/tuiter/index.js
@@ -1,14 +1,23 @@
-import Nav from "../nav";
+import React from "react";
+import ExploreComponent from "./explore";
 import NavigationSidebar from "./navigation-sidebar";
-import WhoToFollowListItem from "./who-to-follow-list/who-to-follow-list-item";
+import WhoToFollowList from "./who-to-follow-list";
 
 function Tuiter() {
   return (
-    <div>
-      <Nav />
-      <NavigationSidebar active="home" />
-      <WhoToFollowListItem />
-      <h1>Tuiter</h1>
+    <div className="row mt-2">
+      <div className="col-2 col-md-2 col-lg-1 col-xl-2">
+        <NavigationSidebar active="explore" />
+      </div>
+      <div
+        className="col-10 col-md-10 col-lg-7 col-xl-6"
+        style={{ position: "relative" }}
+      >
+        <ExploreComponent />
+      </div>
+      <div className="d-sm-none d-md-none d-lg-block col-lg-4 col-xl-4">
+        <WhoToFollowList />
+      </div>
     </div>
   );
 }
