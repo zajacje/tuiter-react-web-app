f364abcfcc7a1fab744bd174cabe03942d55e225
Jennifer Zajac <zajac.je@northeastern.edu>
2022-11-06 18:30:05 -0500

    A7 fix url navigating

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
         <Routes>
           <Route path="/*" element={<Labs />} />
           <Route path="/hello" element={<HelloWorld />} />
-          <Route path="/tuiter" element={<Tuiter />} />
+          <Route path="/tuiter/*" element={<Tuiter />} />
         </Routes>
       </div>
     </BrowserRouter>
